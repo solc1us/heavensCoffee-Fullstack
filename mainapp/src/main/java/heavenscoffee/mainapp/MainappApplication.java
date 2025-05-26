ba59e7504b2d4a17431ed582e544d1b82a9b9409
solc1us <cyan.magina@gmail.com>
2025-05-26 23:17:03 +0700

    first commit

---
+++ mainapp/src/main/java/heavenscoffee/mainapp/MainappApplication.java
@@ -0,0 +1,13 @@
+package heavenscoffee.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainappApplication.class, args);
+	}
+
+}
